d8808dcdca3e411673de7d241e2be3461b588c08
jmisnaza <jmisnaza@hopper.com>
2023-01-13 12:46:11 -0500

    first commit

---
+++ src/main/java/com/spotifyQuipux/spotifyQuipux/SpotifyQuipuxApplication.java
@@ -0,0 +1,13 @@
+package com.spotifyQuipux.spotifyQuipux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifyQuipuxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotifyQuipuxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spotifyQuipux/spotifyQuipux/SpotifyQuipuxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spotifyQuipux.spotifyQuipux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifyQuipuxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
